e359585cfdfc21aa45624bca3ac80e6bedd3273c
venkat987 <venkateshan.k@gmail.com>
2023-04-25 10:32:28 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node!\n'
+  const msg = process.env.DUMMY_VARIABLE_1
   res.end(msg);
 });
 console.log(process.env.testenv);
